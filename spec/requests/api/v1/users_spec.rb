e43430d171b38037c515de2c9bb0946159979e64
Galiwango Ananiya <galiwango101@gmail.com>
2021-08-19 02:59:09 +0300

    Add tests for the update endpoint

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -20,17 +20,25 @@ RSpec.describe "Api::V1::Users", type: :request do
   end
 
   describe "POST /create" do
-    before(:all) do
-      post api_v1_users_url, params: { user: { username: 'testname', email: 'testemail@test.co.ug', password: 'password', password_confirmation: 'password'} } ,as: :json
-      @json_response = JSON.parse(self.response.body)
-    end
-
     it 'should successfully create a user' do
+      post api_v1_users_url, params: { user: { username: 'testname', email: 'testemail@test.co.ug', password: 'password', password_confirmation: 'password'} } ,as: :json
       expect(response).to have_http_status(:created)
     end
 
     it 'should not create a user with a taken email' do
       post api_v1_users_url, params: { user: { username: 'testname2', email: @user.email, password: 'password', password_confirmation: 'password'} } ,as: :json
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+
+  describe "PATCH /update" do
+    it 'should successfully update a user' do
+      patch api_v1_user_url(@user), params: { user: { username: 'testname', email: 'testedmail@test.co.ug', password: 'password', password_confirmation: 'password'} } ,as: :json
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'should not update user when invalid params are sent' do
+      patch api_v1_user_url(@user), params: { user: { username: 'testname2', email: 'wrong_email', password: 'password', password_confirmation: 'password'} } ,as: :json
       expect(response).to have_http_status(:unprocessable_entity)
     end
   end
