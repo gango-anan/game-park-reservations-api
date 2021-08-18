7c5df7487b9d1027a01ceb3c5c745ac61688a85d
Galiwango Ananiya <galiwango101@gmail.com>
2021-08-19 02:25:58 +0300

    Add the user create endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,4 +2,20 @@ class Api::V1::UsersController < ApplicationController
   def show
     render json: User.find(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
 end
