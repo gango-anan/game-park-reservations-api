92bfe0a37860b5cebe97d97485819c67e71bdf87
Galiwango Ananiya <galiwango101@gmail.com>
2021-08-18 18:53:05 +0300

    Add User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true
+  validates :email, uniqueness: true
+  validates_format_of :email, with: /@/
+  validates :password_digest, presence: true
 end
