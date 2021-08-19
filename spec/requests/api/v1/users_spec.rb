25d8a7757b3df3b7b3330cdaf134d78f2fd8d051
Galiwango Ananiya <galiwango101@gmail.com>
2021-08-19 03:43:19 +0300

    Add tests for the user destroy end point

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe "Api::V1::Users", type: :request do
   before(:all) do
     @user = create(:user)
+    @user_two = create(:user)
   end
+
   describe "GET /show" do
     before(:each) do
       get api_v1_user_url(@user), as: :json
@@ -25,6 +27,12 @@ RSpec.describe "Api::V1::Users", type: :request do
       expect(response).to have_http_status(:created)
     end
 
+    it 'should increase the number of users by 1' do
+      expect do
+        post api_v1_users_url, params: { user: { username: 'testname2', email: 'testemail2@test.co.ug', password: 'password', password_confirmation: 'password'} } ,as: :json
+      end.to change(User, :count).by(1)
+    end
+
     it 'should not create a user with a taken email' do
       post api_v1_users_url, params: { user: { username: 'testname2', email: @user.email, password: 'password', password_confirmation: 'password'} } ,as: :json
       expect(response).to have_http_status(:unprocessable_entity)
@@ -40,6 +48,19 @@ RSpec.describe "Api::V1::Users", type: :request do
     it 'should not update user when invalid params are sent' do
       patch api_v1_user_url(@user), params: { user: { username: 'testname2', email: 'wrong_email', password: 'password', password_confirmation: 'password'} } ,as: :json
       expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+
+  describe "DELETE /destroy" do
+    it 'should successfully delete a user' do
+      delete api_v1_user_url(@user), as: :json
+      expect(response).to have_http_status(:no_content)
+    end
+
+    it 'should reduce the number of users by 1' do
+      expect do
+        delete api_v1_user_url(@user_two), as: :json
+      end.to change(User, :count).by(-1)
     end
   end
 end
