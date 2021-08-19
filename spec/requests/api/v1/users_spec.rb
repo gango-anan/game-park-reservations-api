5415f8c6d9c3cc299a1cdc195a2a2f93f4892c62
Galiwango Ananiya <galiwango101@gmail.com>
2021-08-19 15:49:30 +0300

    Add tests for authorization functionality

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "Api::V1::Users", type: :request do
   before(:all) do
     @user = create(:user)
     @user_two = create(:user)
+    @user_three = create(:user)
   end
 
   describe "GET /show" do
@@ -40,27 +41,47 @@ RSpec.describe "Api::V1::Users", type: :request do
   end
 
   describe "PATCH /update" do
-    it 'should successfully update a user' do
-      patch api_v1_user_url(@user), params: { user: { username: 'testname', email: 'testedmail@test.co.ug', password: 'password', password_confirmation: 'password'} } ,as: :json
+    it 'should update user' do
+      patch api_v1_user_url(@user),
+        params: { user: { username: @user.username, email: @user.email, password: 'newpassword', password_confirmation: 'newpassword'} },
+        headers: { Authorization: JsonWebToken.encode(user_id: @user.id) },
+        as: :json
       expect(response).to have_http_status(:success)
     end
 
     it 'should not update user when invalid params are sent' do
-      patch api_v1_user_url(@user), params: { user: { username: 'testname2', email: 'wrong_email', password: 'password', password_confirmation: 'password'} } ,as: :json
+      patch api_v1_user_url(@user),
+      params: { user: { email: 'wrong_email' } },
+      headers: { Authorization: JsonWebToken.encode(user_id: @user.id) },
+      as: :json
       expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+    it 'should forbid unauthorized user from performing update' do
+      patch api_v1_user_url(@user), params: { user: { email: @user.email } }, as: :json
+      expect(response).to have_http_status(:forbidden)
     end
   end
 
   describe "DELETE /destroy" do
     it 'should successfully delete a user' do
-      delete api_v1_user_url(@user), as: :json
+      delete api_v1_user_url(@user), 
+        headers: { Authorization: JsonWebToken.encode(user_id: @user.id) },
+        as: :json
       expect(response).to have_http_status(:no_content)
     end
 
     it 'should reduce the number of users by 1' do
       expect do
-        delete api_v1_user_url(@user_two), as: :json
+        delete api_v1_user_url(@user_two),
+        headers: { Authorization: JsonWebToken.encode(user_id: @user_two.id) },
+        as: :json
       end.to change(User, :count).by(-1)
+    end
+
+    it 'should forbid unauthorized user from deleting a user' do
+      delete api_v1_user_url(@user_three)
+      expect(response).to have_http_status(:forbidden)
     end
   end
 end
