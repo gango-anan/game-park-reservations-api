e4c85817a8163aab01cd9340dbb2c842fba197b4
Galiwango Ananiya <galiwango101@gmail.com>
2021-08-22 14:30:03 +0300

    Add route for the user index action and add all resources for reservations

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       post 'register', to: 'users#create'
-      resources :users, only: %i[show update destroy]
+      resources :users, only: %i[index show update destroy]
       resources :tokens, only: [:create]
       resources :activities
+      resources :reservations
     end
   end
 end
