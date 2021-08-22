7751b602119515041c06561ead8c29b6f4c365cd
Galiwango Ananiya <galiwango101@gmail.com>
2021-08-23 01:50:19 +0300

    Add MainController

---
+++ app/controllers/MainController.rb
@@ -0,0 +1,5 @@
+class MainController < ApplicationController
+  def index
+    render json: 'Welcome to Game Safari Reservations'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'main#index'
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       post 'register', to: 'users#create'
