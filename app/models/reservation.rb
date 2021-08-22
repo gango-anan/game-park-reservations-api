f173735566b718acc49fe4c33f0aac0f2b9d6aff
Galiwango Ananiya <galiwango101@gmail.com>
2021-08-22 16:46:18 +0300

    Refactor to add validation error message to the Reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,5 @@ class Reservation < ApplicationRecord
   belongs_to :activity
 
   validates :reservation_date, presence: true
+  validates_uniqueness_of :activity, scope: :user_id, message: 'You have already reserved this activity'
 end
